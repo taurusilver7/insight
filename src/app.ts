730df237d079c3688e12ca7baa36bbd727f6052d
Devi prasad <prasad20697@gmail.com>
2024-03-04 21:45:14 +0530

    morgan+helmet

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Express } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app: Express = express();
 
 app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
 app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
