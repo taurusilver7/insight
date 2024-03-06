35a9d614309f76edfcfcce4d0071a44297ef2815
Devi prasad <prasad20697@gmail.com>
2024-03-06 16:49:14 +0530

    database connected to application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import * as dotenv from 'dotenv';
-import app from './app';
+import * as dotenv from "dotenv";
+import app from "./app";
+import { AppDataSource } from "./db/data-source";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+AppDataSource.initialize()
+	.then(async () => {
+		console.log("Database connected successfully");
+	})
+	.catch((err) => console.error(err));
+
 app.listen(PORT, () => {
-   console.log(`Server running on port: ${PORT}`)
-})
+	console.log(`Server running on port: ${PORT}`);
+});
