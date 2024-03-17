2430c0d4d882fce40bd16df8e08e8f7b971863e6
Devi prasad <prasad20697@gmail.com>
2024-03-17 16:26:31 +0530

    get author controller

--- src/controllers/author.ts
+++ src/controllers/author.ts
@@ -3,13 +3,26 @@ import { AppDataSource } from "../db/data-source";
 import { Author } from "../entities/author";
 
 export class AuthorsController {
-	async getAuthor(req: Request, res: Response) {
+	async getAuthors(req: Request, res: Response) {
 		const authors = await AppDataSource.getRepository(Author).find();
 
 		return res.status(200).json({
 			success: true,
 			message: "Fetched authors successfully",
 			data: authors,
+		});
+	}
+
+	async getAuthor(req: Request, res: Response) {
+		const { id } = req.params;
+
+		const author = await AppDataSource.getRepository(Author).findOneByOrFail({
+			id: Number(id),
+		});
+		return res.status(200).json({
+			success: true,
+			message: "Fetched author successfully",
+			data: author,
 		});
 	}
 }
