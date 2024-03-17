2a2e013af5f7e9f213a8dbd2d41d92804a5cb669
Devi prasad <prasad20697@gmail.com>
2024-03-17 22:29:08 +0530

    response util fn

---
+++ src/utils/response.ts
@@ -0,0 +1,31 @@
+import { Response } from "express";
+
+export class ResponseUtil {
+	static sendResponse<T>(
+		res: Response,
+		message: string,
+		data: T,
+		paginationInfo: any = null,
+		statusCode = 200
+	): Response<T> {
+		return res.status(statusCode).json({
+			success: true,
+			message,
+			data,
+			paginationInfo,
+		});
+	}
+
+	static sendError<T>(
+		res: Response,
+		message: string,
+		statusCode: number = 500,
+		error: T
+	): Response<T> {
+		return res.status(statusCode).json({
+			success: false,
+			message,
+			error,
+		});
+	}
+}
