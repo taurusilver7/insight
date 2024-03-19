4ca3efb0c4d60f058df4a7ed42f46e53fa2122b8
Devi prasad <prasad20697@gmail.com>
2024-03-19 22:02:52 +0530

    database log modified only for queries

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -5,15 +5,15 @@ import { Author } from "../entities/author";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-	type: "mysql",
-	host: process.env.DB_HOST || "127.0.0.1",
-	port: Number(process.env.DB_PORT) || 3306,
-	username: process.env.DB_USERNAME || "root",
-	password: process.env.DB_PASSWORD || "root",
-	database: process.env.DB_DATABASE || "insight_primary",
-	logging: true,
-	synchronize: false,
-	entities: [Author],
-	subscribers: [],
-	migrations: ["src/db/migrations/*.ts"],
+  type: "mysql",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_DATABASE || "insight_primary",
+  logging: ["query"],
+  synchronize: false,
+  entities: [Author],
+  subscribers: [],
+  migrations: ["src/db/migrations/*.ts"],
 });
