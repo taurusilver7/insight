ac072f08fc72bc7af06774608e57968fffd171bd
Devi prasad <prasad20697@gmail.com>
2024-03-19 23:28:21 +0530

    author post route

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { AuthorsController } from "../controllers/author";
 import { ErrorHandler } from "../utils/error-handler";
+import { FileUploader } from "../middleware/file-upload";
 
 const router = express.Router();
 const authorController = new AuthorsController();
 
 router.get("/", ErrorHandler.handleError(authorController.getAuthors));
 router.get("/:id", ErrorHandler.handleError(authorController.getAuthor));
-router.post("/", ErrorHandler.handleError(authorController.createAuthor));
+router.post(
+  "/",
+  FileUploader.upload("image", "author", 2 * 1024 * 1024),
+  ErrorHandler.handleError(authorController.createAuthor)
+);
 
 export default router;
