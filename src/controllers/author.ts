55293de38fe6b198f96c83f92c805ef787047a1d
Devi prasad <prasad20697@gmail.com>
2024-03-19 21:37:10 +0530

    pagination in author controller init

--- src/controllers/author.ts
+++ src/controllers/author.ts
@@ -5,22 +5,18 @@ import { ResponseUtil } from "../utils/response";
 import { AppDataSource } from "../db/data-source";
 
 export class AuthorsController {
-	async getAuthors(req: Request, res: Response) {
-		const authors = await AppDataSource.getRepository(Author).find();
+  async getAuthors(req: Request, res: Response) {
+    const authors = await AppDataSource.getRepository(Author).createQueryBuilder().orderBy("id", "DESC");
 
-		return ResponseUtil.sendResponse(res, "Authors fetched!", authors);
-	}
+    return ResponseUtil.sendResponse(res, "Authors fetched!", authors, null);
+  }
 
-	async getAuthor(req: Request, res: Response) {
-		const { id } = req.params;
+  async getAuthor(req: Request, res: Response) {
+    const { id } = req.params;
 
-		const author = await AppDataSource.getRepository(Author).findOneByOrFail({
-			id: Number(id),
-		});
-		return ResponseUtil.sendResponse<Author>(
-			res,
-			"Author info fetched",
-			author
-		);
-	}
+    const author = await AppDataSource.getRepository(Author).findOneByOrFail({
+      id: Number(id),
+    });
+    return ResponseUtil.sendResponse<Author>(res, "Author info fetched", author);
+  }
 }
