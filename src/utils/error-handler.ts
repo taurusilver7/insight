95327e93af3d24dd9dcb208eb9c2fb0f72cb1151
Devi prasad <prasad20697@gmail.com>
2024-03-19 23:28:33 +0530

    format error handler

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,9 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 
 export class ErrorHandler {
-	static handleError(fn) {
-		return (req: Request, res: Response, next: NextFunction) => {
-			Promise.resolve(fn(req, res, next).catch(next));
-		};
-	}
+  static catchErrors(fn) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next).catch(next));
+    };
+  }
+  static handleError(fn) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next).catch(next));
+    };
+  }
+
+  static formatError(err: any) {
+    const errors = {};
+    err.forEach((e) => {
+      if (!errors[e.property]) {
+        errors[e.property] = [];
+      }
+      errors[e.property].push(e.constraints[Object.keys(e.constraints)[0]]);
+    });
+    return errors;
+  }
 }
