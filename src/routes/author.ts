d5cfa2685f264dee0e0da4172550ef9143c1a7f9
Devi prasad <prasad20697@gmail.com>
2024-03-19 22:47:11 +0530

    create author route

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -5,7 +5,8 @@ import { ErrorHandler } from "../utils/error-handler";
 const router = express.Router();
 const authorController = new AuthorsController();
 
-router.get("/", authorController.getAuthors);
+router.get("/", ErrorHandler.handleError(authorController.getAuthors));
 router.get("/:id", ErrorHandler.handleError(authorController.getAuthor));
+router.post("/", ErrorHandler.handleError(authorController.createAuthor));
 
 export default router;
