c1a63baacd9e5a05ae8ec1bcffc75c79433eb9b1
Devi prasad <prasad20697@gmail.com>
2024-03-19 23:27:48 +0530

    create author controller init

--- src/controllers/author.ts
+++ src/controllers/author.ts
@@ -7,7 +7,7 @@ import { Paginator } from "../db/pagination";
 
 export class AuthorsController {
   async getAuthors(req: Request, res: Response) {
-    const builder = await AppDataSource.getRepository(Author).createQueryBuilder().orderBy("id", "DESC");
+    const builder = AppDataSource.getRepository(Author).createQueryBuilder().orderBy("id", "DESC");
     const { records: authors, paginationInfo } = await Paginator.paginate(builder, req);
 
     return ResponseUtil.sendResponse(res, "Authors fetched!", authors, paginationInfo);
@@ -21,12 +21,13 @@ export class AuthorsController {
     });
     return ResponseUtil.sendResponse<Author>(res, "Author info fetched", author);
   }
-  async createAuthor(req: Request, res: Response) {
-    const { id } = req.params;
+  async createAuthor(req: Request, res: Response): Promise<Response> {
+    const authorData = req.body;
 
-    const author = await AppDataSource.getRepository(Author).findOneByOrFail({
-      id: Number(id),
-    });
-    return ResponseUtil.sendResponse<Author>(res, "Author info fetched", author);
+    const repo = AppDataSource.getRepository(Author);
+    const author = repo.create(authorData);
+    await repo.save(author);
+
+    return ResponseUtil.sendResponse(res, "Author created successfully!", author, 200);
   }
 }
