8cf901963a127c91c0eb76d936a209c972dc37be
Devi prasad <prasad20697@gmail.com>
2024-03-19 23:28:02 +0530

    create autor dto validator

---
+++ src/dto/create-author.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateAuthorDTO {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  bio: string;
+}
