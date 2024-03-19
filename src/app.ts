0261b24572b6bb6be0606a005201187d1874265d
Devi prasad <prasad20697@gmail.com>
2024-03-19 21:10:05 +0530

    error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
-
-import * as middleware from "./middleware";
+import { ResponseUtil } from "./utils/response";
 import mainRoute from "./routes/index";
+
+import { EntityNotFoundError } from "typeorm";
 
 const app: Express = express();
 
@@ -19,10 +20,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/v1", mainRoute);
 
 app.use("*", (req: Request, res: Response) => {
-	res.status(404).json({
-		success: false,
-		message: "Invalid route!",
-	});
+  res.status(404).json({
+    success: false,
+    message: "Invalid route!",
+  });
+});
+
+// define a middleware function to handle errors
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof EntityNotFoundError) {
+    return ResponseUtil.sendError(res, 404, err);
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Not Found!!",
+  });
 });
 
 export default app;
