8cad33b8fe66187c4c77afd1738e89fd198b446a
Devi prasad <prasad20697@gmail.com>
2024-03-20 21:47:09 +0530

    author routes updated

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import { AuthorsController } from "../controllers/author";
-import { ErrorHandler } from "../utils/error-handler";
+import { ErrorHandler } from "../middleware/error-handler";
 import { FileUploader } from "../middleware/file-upload";
 
 const router = express.Router();
 const authorController = new AuthorsController();
 
-router.get("/", ErrorHandler.handleError(authorController.getAuthors));
-router.get("/:id", ErrorHandler.handleError(authorController.getAuthor));
+router.get("/", ErrorHandler.catchError(authorController.getAuthors));
+router.get("/:id", ErrorHandler.catchError(authorController.getAuthor));
 router.post(
   "/",
   FileUploader.upload("image", "author", 2 * 1024 * 1024),
-  ErrorHandler.handleError(authorController.createAuthor)
+  ErrorHandler.catchError(authorController.createAuthor)
 );
+router.put("/:id", ErrorHandler.catchError(authorController.updateAuthor));
+router.delete("/:id", ErrorHandler.catchError(authorController.deleteAuthor));
 
 export default router;
