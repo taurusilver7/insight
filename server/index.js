812d4760df895222e49a1653f2bdc7349f3f81f1
Devi prasad <prasad20697@gmail.com>
2025-06-01 22:34:12 +0530

    root server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("MongoDb connected, ✅");
+		app.listen(port, () =>
+			console.log(`Server connected at http://localhost:${port}`)
+		);
+	})
+	.catch((err) => console.error("MonogoDB Error,❌", err));
+ 
